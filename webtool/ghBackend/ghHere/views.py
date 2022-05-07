6a31d71ae093aba8e73ab05e924599e30655ebab
Hao Zheng <46986371+HaoZhengUPenn@users.noreply.github.com>
2022-05-07 12:47:38 -0400

    webtool

--- webtool/ghBackend/ghHere/views.py
+++ webtool/ghBackend/ghHere/views.py
@@ -26,19 +26,47 @@ from rest_framework.response import Response
 
 from .models import GHRequest
 
+from django.core.mail import EmailMessage
+def send_email(request):
+    global emailaddress
+    msg = EmailMessage(
+        subject='Wing Structure Generator | PSL - Your result is ready!',
+        body='Hello,\n\nThank you for using Wing Structure Generator from Polyhedral Structures Laboratory, University of Pennsylvania. Your structure has been successfully generated. Please download the data file (.CSV) in the attachment. You can upload it in the "File Manager" panel to restore the result. Have fun!\n\nBest,\nHao Zheng\nPh.D. Candidate at UPenn',
+        from_email="359580720@qq.com",  # 也可以从settings中获取
+        to=[emailaddress]
+    )
+    msg.attach_file(r'media/Default/output.csv')
+    msg.send(fail_silently=False)
+    return HttpResponse('OK')
+
 
 class PredictView(views.APIView):
     def post(self, request, endpoint_name, format=None):
         prediction = {}
         prediction['status'] = 'uncompleted'
         
-        with open(r'media/Default/done.txt', "r") as f:
-            data = f.readlines()
-            if int(data[0].strip('\n'))==0:
-                return Response(prediction)
-        
-        with open(r'media/Default/done.txt', 'w') as f:
-            f.write('0'+'\n')
+        fem = True
+        for param in request.data.values():
+            if str(param)[2]!="F":
+                fem = False
+            global emailaddress
+            emailaddress = str(param).split(',')[-1][2:-2]
+            print("email to",emailaddress)
+            break
+        if fem==False:
+            with open(r'media/Default/done.txt', "r") as f:
+                data = f.readlines()
+                if int(data[0].strip('\n'))==0:
+                    return Response(prediction)
+            with open(r'media/Default/done.txt', 'w') as f:
+                f.write('0'+'\n')
+        if fem==True:
+            with open(r'media/Default/done_FEM.txt', "r") as f:
+                data = f.readlines()
+                if int(data[0].strip('\n'))==0:
+                    return Response(prediction)
+            with open(r'media/Default/done_FEM.txt', 'w') as f:
+                f.write('0'+'\n')
         
         delay = int(self.request.query_params.get("delay", 60*15))
         interval = int(self.request.query_params.get("interval", 15))
@@ -65,57 +93,71 @@ class PredictView(views.APIView):
         if not os.path.exists(os.getcwd()+r'/media/'+code):
             os.makedirs(os.getcwd()+r'/media/'+code)
         # print(gh_request.input_data)
-        with open(r'media/Default/input.txt', 'w') as f:
-            for param in request.data.values():
-                f.write(str(param)+'\n')
+        
+        if fem==False:
+            with open(r'media/Default/input.txt', 'w') as f:
+                for param in request.data.values():
+                    f.write(str(param)+'\n')
+        if fem==True:
+            with open(r'media/Default/input_FEM.txt', 'w') as f:
+                for param in request.data.values():
+                    f.write(str(param)+'\n')
         print('Recorded!!!')
-
-        try:
-            os.remove(r'media/Default/output.csv')
-        except:
-            pass
-        for i in range(0, int(delay/interval)+1):
-            print('Round',i)
+        
+        if fem==False:
             try:
-                #shutil.copyfile(r'media/AIStructureGenerator_User_EFC_APW_Web_output.stl', r'media/Default/nnnnnnn.stl')#gan
-                fn_all = os.listdir(r'media/Default/')
-                #stl
-                fn_stl = []
-                for fn in fn_all:
-                    if fn.endswith(".csv"):
-                        fn_stl.append(fn)
-                fn_stl.sort(key=lambda fn:os.path.getmtime(r'media/Default/'+fn))
-                print(fn_stl[-1])
-                shutil.copyfile(r'media/Default/'+ fn_stl[-1], r'media/Default/output.csv')#show
-                shutil.move(r'media/Default/'+ fn_stl[-1], r'media/'+code+r'/'+ fn_stl[-1])#bake
-                gh_request.gh_num = fn_stl[-1].split('.')[0]
-                gh_request.stl_result = code+r'/'+ fn_stl[-1]
-                gh_request.succeed = True
-                gh_request.save(update_fields=['gh_num','stl_result','succeed'])
-                prediction['fp'] = code+r'/'+ fn_stl[-1]
-                print('Generated !!!')
-                #txt
-                # fn_txt = []
-                # for fn in fn_all:
-                #     if fn.endswith(".txt"):
-                #         fn_txt.append(fn)
-                # fn_txt.sort(key=lambda fn:os.path.getmtime(r'media/Default/'+fn))
-                shutil.copyfile(r'media/Default/input.txt', r'media/'+code+r'/input.txt')
-                time.sleep(interval)
-                
-                gh_request.txt_input = code+r'/input.txt'
-                gh_request.save(update_fields=['txt_input'])
-                #done
-                # try:
-                #     time.sleep(600)
-                #     os.remove(r'media/Default/output.stl')
-                # except:
-                #     pass
-
-                prediction['status'] = 'completed'
-                break
+                os.remove(r'media/Default/output.csv')
             except:
                 pass
-            time.sleep(interval)
+            for i in range(0, int(delay/interval)+1):
+                print('Round',i)
+                try:
+                    #shutil.copyfile(r'media/AIStructureGenerator_User_EFC_APW_Web_output.stl', r'media/Default/nnnnnnn.stl')#gan
+                    fn_all = os.listdir(r'media/Default/')
+                    #stl
+                    fn_stl = []
+                    for fn in fn_all:
+                        if fn.endswith(".csv"):
+                            fn_stl.append(fn)
+                    fn_stl.sort(key=lambda fn:os.path.getmtime(r'media/Default/'+fn))
+                    print(fn_stl[-1])
+                    shutil.copyfile(r'media/Default/'+ fn_stl[-1], r'media/Default/output.csv')#show
+                    shutil.move(r'media/Default/'+ fn_stl[-1], r'media/'+code+r'/'+ fn_stl[-1])#bake
+                    gh_request.gh_num = fn_stl[-1].split('.')[0]
+                    gh_request.stl_result = code+r'/'+ fn_stl[-1]
+                    gh_request.succeed = True
+                    gh_request.save(update_fields=['gh_num','stl_result','succeed'])
+                    prediction['fp'] = code+r'/'+ fn_stl[-1]
+                    print('Generated !!!')
+                    #txt
+                    # fn_txt = []
+                    # for fn in fn_all:
+                    #     if fn.endswith(".txt"):
+                    #         fn_txt.append(fn)
+                    # fn_txt.sort(key=lambda fn:os.path.getmtime(r'media/Default/'+fn))
+                    shutil.copyfile(r'media/Default/input.txt', r'media/'+code+r'/input.txt')
+                    time.sleep(interval)
+                    
+                    gh_request.txt_input = code+r'/input.txt'
+                    gh_request.save(update_fields=['txt_input'])
+                    #done
+                    # try:
+                    #     time.sleep(600)
+                    #     os.remove(r'media/Default/output.stl')
+                    # except:
+                    #     pass
+
+                    prediction['status'] = 'completed'
+                    break
+                except:
+                    pass
+                time.sleep(interval)
+                
+            send_email(request)
+            
+        if fem==True:
+            with open(r'media/Default/outputfem.txt', 'w') as f:
+                f.write('0\n')
+            prediction['status'] = 'completed'
 
         return Response(prediction)
